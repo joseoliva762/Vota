e9bea4d84dce7efe11c7074bb9539720aad04705
Joseoliva762 <joseolivacr@unimagdalena.edu.co>
2022-01-22 13:17:00 -0500

    logo updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import { NavUserOutlet } from "components/NavUserOutlet";
-import logo from 'public/icons/pointer.svg';
+import logo from 'public/icons/logo.svg';
 
 export default function Header () {
     return (
         <>
             <header className="bg-js z-20 flex justify-between w-full max-w-4xl mx-auto px-4 py-2 items-center sticky top-0 cursor-default border-b-0 border-black md:border-b-2">
                 {/* <h1 className="text-xl font-bold">🗳️ Vota.Dev</h1> */}
-                <Image src={logo} height={40} width={40} className="hover:cursor-pointer"/>
+                <Image src={logo} height={40} width={60} className="hover:cursor-pointer"/>
                 <div>
                     <NavUserOutlet />
                 </div>
