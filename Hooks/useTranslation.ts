e477342ee9f23ab8247f257b292294f0aea982b0
Joseoliva762 <joseolivacr@unimagdalena.edu.co>
2022-01-22 20:50:12 -0500

    t2f

--- Hooks/useTranslation.ts
+++ Hooks/useTranslation.ts
@@ -1,9 +1,8 @@
 import { useRouter } from 'next/router';
 import { en, es } from 'translation';
 
-
-
 export const useTranslations = () => {
     const { locale } = useRouter();
+    console.log({ locale, en, es})
     return locale === 'es' ? es : en;
 }
